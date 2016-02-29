8c4450dccba7dc38a7171598331de6c27d4ea2d7
Kevin Huynh <kevin@kevinhuynh.net>
2016-02-28 21:55:52 -0800

    Added DataModel for VPN request

--- IT_product_log/IT_product_log/Models/Model.cs
+++ IT_product_log/IT_product_log/Models/Model.cs
@@ -9,13 +9,37 @@ namespace IT_product_log.Models
 {
     public class Model
     {
-        public int id { get; set; }
+        public string Company_Name { get; set; }
+        public string Company_Other { get; set; }
+        public string Machine_Owner { get; set; }
+        public string Manager { get; set; }
+        public string Office_Address { get; set; }
+        public string Office_Location { get; set; }
+        public string Radius_Profile_Other { get; set; }
+        public string Radius_Profile_Select { get; set; }
+        public string Systems_List { get; set; }
+        public DateTime VPN_accessEnd { get; set; }
+        public DateTime VPN_accessStart { get; set; }
+        public string VPN_justification { get; set; }
+        public string VPN_profileSelect { get; set; }
+        public string VPN_recipientEmail { get; set; }
+        public string VPN_recipientFirst { get; set; }
+        public string VPN_recipientLast { get; set; }
+        public int VPN_requestID { get; set; }
+        public string VPN_requestStatus { get; set; }
+        public int VPN_userCode { get; set; }
+        public string VPN_userDept { get; set; }
+        public string VPN_userStatus { get; set; }
+        public PhoneAttribute Work_Phone { get; set; }
+        public string VPN_requestor { get; set; }
 
+
+
+        public int id { get; set; }
         [Required(ErrorMessage = "Please enter a code")]
         public string Code { get; set; }
         [Required(ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
-
         public string Category { get; set;}
         public string Description { get; set; }
     }
