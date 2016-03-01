b0161bb5837f58e63252b0546e8c6700999e5ff8
Kevin Huynh <kevin@kevinhuynh.net>
2016-02-29 21:46:00 -0800

    Added FormsController and linked within portal to VPNrequest view

---
+++ IT_product_log/IT_product_log/Controllers/FormsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_product_log.Controllers
+{
+    public class FormsController : Controller
+    {
+        public ViewResult VPNRequest()
+        {
+            return View();
+        }
+    }
+}
