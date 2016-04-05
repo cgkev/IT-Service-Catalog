ba37b580703df449174efae5ecf47855f727cb21
rwu864 <rwu864@gmail.com>
2016-04-04 23:47:56 -0700

    Added the "create" functionality
    To do:
    - Add authentication (and created by)
    - Refactor code so only internal names are used (and not both internal
    names and column names)

--- IT_product_log/IT_product_log/Models/SpConnectionVPN.cs
+++ IT_product_log/IT_product_log/Models/SpConnectionVPN.cs
@@ -34,6 +34,25 @@ namespace IT_product_log.Models
         string endDate = "VPN Access End";
         string companyOther = "Company Other";
 
+        //internal names 
+        string internalVpnRecipientFirst = "VPN_x0020_Recipient_x0020_First";
+        string internalVpnRecipientLast = "VPN_x0020_Recipient_x0020_Last";
+        string internalWorkPhone = "Work_x0020_Phone";
+        string internalEmail = "VPN_x0020_Recipient_x0020_Email";
+        string internalUserCode = "VPN_x0020_User_x0020_Code";
+        string internalManager = "Manager";
+        string internalSystemsList = "Systems_x0020_List";
+        string internalJustification = "VPN_x0020_Justification";
+        string internalAccessStart = "VPN_x0020_Access_x0020_Start";
+        string internalAccessEnd = "VPN_x0020_Access_x0020_End";
+        string internalUserDept = "VPN_x0020_User_x0020_Department";
+        string internalCompanyName = "NewColumn1";
+        string internalCompanyOther = "Company_x0020_Other";
+        string internalOfficeLocation = "QTC_x0020_Office_x0020_Location";
+        string internalOfficeAddress = "QTC_x0020_Office_x0020_Select";
+        string internalMachineOwner = "NewColumn10";
+        string internalUserStatus = "VPN_x0020_User_x0020_Status";
+
 
         public SpConnectionVPN()
         {
@@ -97,28 +116,26 @@ namespace IT_product_log.Models
             List spList = clientContext.Web.Lists.GetByTitle(ListName);
             clientContext.Load(spList);
 
-            ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
-            ListItem listItem = spList.AddItem(itemCreateInfo);
+            var itemCreateInfo = new ListItemCreationInformation();
+            var listItem = spList.AddItem(itemCreateInfo);
 
-            Console.WriteLine(input.VPN_recipientFirst);
-            Console.WriteLine(input.VPN_recipientLast);
-
-            listItem[spList.Fields.GetByTitle(vpnRecipientFirst).InternalName] = input.VPN_recipientFirst;
-            listItem[spList.Fields.GetByTitle(vpnRecipientLast).InternalName] = input.VPN_recipientLast;
-            listItem[spList.Fields.GetByTitle(workPhone).InternalName] = input.Work_Phone;
-            listItem[spList.Fields.GetByTitle(email).InternalName] = input.VPN_recipientEmail;
-            listItem[spList.Fields.GetByTitle(userCode).InternalName] = input.VPN_userCode;
-            listItem[spList.Fields.GetByTitle(manager).InternalName] = input.Manager;
-            listItem[spList.Fields.GetByTitle(systemsList).InternalName] = input.Systems_List;
-            listItem[spList.Fields.GetByTitle(vpnJustification).InternalName] = input.VPN_justification;
-            listItem[spList.Fields.GetByTitle(startDate).InternalName] = input.VPN_accessStart;
-            listItem[spList.Fields.GetByTitle(endDate).InternalName] = input.VPN_accessEnd;
-            listItem[spList.Fields.GetByTitle(deptName).InternalName] = input.VPN_userDept;
-            listItem[spList.Fields.GetByTitle(companyName).InternalName] = input.Company_Name;
-            listItem[spList.Fields.GetByTitle(companyOther).InternalName] = input.Company_Other;
-            listItem[spList.Fields.GetByTitle(qtcOfficeLocation).InternalName] = input.Office_Location;
-            listItem[spList.Fields.GetByTitle(qtcofficeSelect).InternalName] = input.Office_Address;
-            listItem[spList.Fields.GetByTitle(machineOwner).InternalName] = input.Machine_Owner;
+            listItem[internalVpnRecipientFirst] = input.VPN_recipientFirst;
+            listItem[internalVpnRecipientLast] = input.VPN_recipientLast;
+            listItem[internalWorkPhone] = input.Work_Phone;
+            listItem[internalEmail] = input.VPN_recipientEmail;
+            listItem[internalUserCode] = input.VPN_userCode;
+            listItem[internalManager] = input.Manager;
+            listItem[internalSystemsList] = input.Systems_List;
+            listItem[internalJustification] = input.VPN_justification;
+            listItem[internalAccessStart] = input.VPN_accessStart;
+            listItem[internalAccessEnd] = input.VPN_accessEnd;
+            listItem[internalUserDept] = input.VPN_userDept;
+            listItem[internalCompanyName] = input.Company_Name;
+            listItem[internalCompanyOther] = input.Company_Other;
+            listItem[internalOfficeLocation] = input.Office_Location;
+            listItem[internalOfficeAddress] = input.Office_Address;
+            listItem[internalMachineOwner] = input.Machine_Owner;
+            listItem[internalUserStatus] = input.VPN_userStatus;
 
             listItem.Update();
             clientContext.ExecuteQuery();
